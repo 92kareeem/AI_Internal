c828237b2a7bf9efa16300213fb5bea48e7f133b
Syed Abdul Kareem Ahmed <160421748048@mjcollege.ac.in>
2024-02-05 21:55:51 +0530

    Update Pos_tagging.py

--- Pos_tagging.py
+++ Pos_tagging.py
@@ -1,18 +1,8 @@
 import nltk
-
-# Download the missing resource
 nltk.download('averaged_perceptron_tagger')
-
-# Now, you should be able to use the pos_tag function without errors
 from nltk import pos_tag
 from nltk.tokenize import word_tokenize
-
-# Example text
 text = "the cat sat on the mat."
-
-# Tokenize the text into words
 words = word_tokenize(text)
-
-# Part-of-speech tagging
 tagged_words = pos_tag(words)
 print("Part-of-Speech Tags:", tagged_words)
