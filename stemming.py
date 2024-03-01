cfda670e18c0db6cac56f4712bf045618c82640a
Syed Abdul Kareem Ahmed <160421748048@mjcollege.ac.in>
2024-03-01 09:30:09 +0530

    Update stemming.py

--- stemming.py
+++ stemming.py
@@ -4,19 +4,14 @@ from nltk.stem import PorterStemmer
 
 nltk.download('punkt')
 
-# Example text
 text = "programming  programmed."
-
-# Tokenize the text into words
+
 words = word_tokenize(text)
-
-# Create a Porter stemmer
+
 stemmer = PorterStemmer()
-
-# Apply stemming to each word
+
 stemmed_words = [stemmer.stem(word) for word in words]
-
-# Print the results
+
 print("Original Words:")
 print(words)
 
